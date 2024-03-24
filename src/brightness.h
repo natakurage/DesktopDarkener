ef2651d6dd3f70b3010826d064f8ca5e12ce1a31
natakurage <uzogamuzol@gmail.com>
2024-03-25 00:18:02 +0900

    initial commit

---
+++ src/brightness.h
@@ -0,0 +1,56 @@
+#pragma comment(lib, "user32.lib")
+#pragma comment(lib, "gdi32.lib")
+#include <windows.h>
+#include <cmath>
+#include <iostream>
+
+const int MAX_VALUE = 65535;
+const int MIN_VALUE = 32768;
+const int MAX_TRIES = 20;
+
+BOOL set_monitor_brightness(HDC hdc, float brightness, float gamma)
+{
+    if (brightness < 0 && 1 < brightness)
+    {
+        throw "brightness must be 0 to 1";
+    }
+    float coef = brightness * (MAX_VALUE - MIN_VALUE) + MIN_VALUE;
+    WORD ramp[3][256];
+    for (int color = 0; color < 3; color++)
+    {
+        for (int x = 0; x < 256; x++)
+        {
+            ramp[color][x] = WORD(coef * x / 256);
+        }
+    }
+    int result = SetDeviceGammaRamp(hdc, ramp);
+    return result;
+}
+
+BOOL CALLBACK MyMonitorEnumProc(HMONITOR hMonitor, HDC hdcMonitor, LPRECT lprcMonitor, LPARAM dwData)
+{
+    float brightness = float(dwData) / 100;
+    float gamma = 1.0;
+    BOOL result = set_monitor_brightness(hdcMonitor, brightness, gamma);
+    std::cout << (result ? "brightness ok" : "brightness failed") << std::endl;
+    return result;
+}
+
+BOOL set_brightness(int brightness)
+{
+    for (int tries = 0; tries < MAX_TRIES; tries++)
+    {
+        HDC AllDC = GetDC(NULL);
+        bool result = EnumDisplayMonitors(AllDC, NULL, MyMonitorEnumProc, brightness);
+        std::cout << (result ? "enum ok" : "enum failed") << std::endl;
+        bool result2 = ReleaseDC(0, AllDC);
+        std::cout << (result2 ? "released" : "not released") << std::endl;
+        if (result) {
+            return true;
+        } else {
+            std::cout << tries + 1 << "th trial failed, retrying..." << std::endl;;
+        }
+    }
+    std::cout << "Failed" << MAX_TRIES << "times, finished" << std::endl;;
+    return false;
+}
---
+++ src/main.cc
@@ -0,0 +1,13 @@
+#include "brightness.h"
+#include <iostream>
+
+int main (int argc, char *argv[]) {
+    int brightness;
+    if (argc == 0)
+        brightness = 100;
+    else
+        brightness = atoi(argv[1]);
+    std::cout << brightness << std::endl;
+    BOOL result = set_brightness(brightness);
+    return result ? 0 : 1;
+}
